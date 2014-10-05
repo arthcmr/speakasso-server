7bb292c0e0d30e279149cc4aca24c4b25dcee5f5
Arthur Camara <arthurcamara@gmail.com>
2014-10-05 16:30:08 +0200

    Add automated tests

---
+++ app.test.js
@@ -0,0 +1,101 @@
+var superagent = require('superagent')
+var expect = require('expect.js')
+
+describe('Perception API server', function() {
+
+    it('retrieves strings', function(done) {
+        superagent.get('http://localhost:3000/getAll')
+            .end(function(e, res) {
+                expect(e).to.eql(null);
+                expect(typeof res.body).to.eql('object');
+                expect(typeof res.body.i18n).to.eql('object');
+                expect(typeof res.body.experiment).to.eql('object');
+                done();
+            });
+    });
+
+    it('retrieves the translated strings in portuguese', function(done) {
+        superagent.get('http://localhost:3000/getAll')
+            .query({
+                lang: 'portuguese'
+            })
+            .end(function(e, res) {
+                expect(e).to.eql(null);
+                expect(typeof res.body).to.eql('object');
+                expect(typeof res.body.i18n).to.eql('object');
+                expect(typeof res.body.i18n.portuguese).to.eql('object');
+                expect(typeof res.body.experiment).to.eql('object');
+                expect(res.body.i18n.portuguese.words.brick).to.eql('tijolo');
+                done();
+            });
+    });
+
+    it('fails when inserting empty entries', function(done) {
+        superagent.post('http://localhost:3000/insert')
+            .send({
+                entry: {}
+            })
+            .end(function(e, res) {
+                expect(e).to.eql(null);
+                expect(res.body.success).to.eql(false);
+                done();
+            });
+    });
+
+    it('succeeds when inserting expected entries', function(done) {
+        superagent.post('http://localhost:3000/insert')
+            .send({
+                email: "test@test.com",
+                language: "portuguese",
+                responses: [{
+                    id: "turtle",
+                    high: true,
+                    color: true,
+                    correct: 'l',
+                    response: 'l',
+                    time: 387
+                }, {
+                    id: "bird",
+                    high: true,
+                    color: false,
+                    correct: 'l',
+                    response: 'r',
+                    time: 507
+                }]
+            })
+            .end(function(e, res) {
+                expect(e).to.eql(null);
+                expect(typeof res.body).to.eql('object');
+                expect(res.body.success).to.eql(true);
+                done();
+            });
+    });
+
+    it('gets entries from a specific user', function(done) {
+        superagent.get('http://localhost:3000/get')
+            .query({
+                email: 'test@test.com'
+            })
+            .end(function(e, res) {
+                console.log(res.body);
+                expect(e).to.eql(null);
+                expect(typeof res.body).to.eql('object');
+                expect(typeof res.body.success).to.eql('undefined');
+                expect(res.body[0].email).to.eql('test@test.com');
+                done();
+            });
+    });
+
+    it('removes entries from a specific user', function(done) {
+        superagent.del('http://localhost:3000/remove')
+            .send({
+                email: 'test@test.com'
+            })
+            .end(function(e, res) {
+                expect(e).to.eql(null);
+                expect(typeof res.body).to.eql('object');
+                expect(res.body.success).to.eql(true);
+                done();
+            });
+    });
+});
